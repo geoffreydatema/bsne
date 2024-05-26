e3db801f3ab689c0bf78898d876661a82456119e
Geoffrey Datema <geoffrey@geoffreydatema.com>
2024-05-25 23:12:11 -0700

    added stylesheet

--- editor/Editor.py
+++ editor/Editor.py
@@ -1,9 +1,17 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QFile
 
 class Editor(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.init()
 
+    def loadStylesheet(self, filename):
+        file = QFile(filename)
+        file.open(QFile.ReadOnly | QFile.Text)
+        stylesheet = file.readAll()
+        QApplication.instance().setStyleSheet(str(stylesheet, encoding="utf=8"))
+    
     def init(self):
+        self.loadStylesheet("data/EditorStylesheet.qss")
         self.show()
