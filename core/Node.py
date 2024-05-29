2dc734efa0fedab2eb3109aa093a246572e22b48
Geoffrey Datema <geoffrey@geoffreydatema.com>
2024-05-28 22:47:58 -0700

    added basic unit stack idea in BaseNode

--- core/Node.py
+++ core/Node.py
@@ -11,14 +11,16 @@ class BaseNode(QGraphicsItem):
         self.outputs = outputs
         self.tx = 0
         self.ty = 0
-        self.width = 200
-        self.height = 300
         self.radius = 8
-        self.titleHeight = 24
+        self.unitSize = 24
+        self.unitCount = 1
+        self.width = 200
+        self.height = (self.unitSize * 1.5) + (self.unitSize * self.unitCount)
         self.padding = 8
         self.titleColor = QColor("#dddddd")
         self.titleBrush = QBrush(QColor("#444444"))
         self.backgroundBrush = QBrush(QColor("#222222"))
+        self.unitBrush = QBrush(QColor("#333333"))
         self.titleElement = QGraphicsTextItem(self)
         self.outlinePenUnselected = QPen(QColor("#111111"))
         self.outlinePenSelected = QPen(QColor("#cccccc"))
@@ -28,34 +30,47 @@ class BaseNode(QGraphicsItem):
     def boundingRect(self):
         return QRectF(0, 0, self.width, self.height).normalized()
 
-    def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
-        # title
+    def paintTitle(self, painter):
         titlePath = QPainterPath()
         titlePath.setFillRule(Qt.WindingFill)
-        titlePath.addRoundedRect(0, 0, self.width, self.titleHeight, self.radius, self.radius)
-        titlePath.addRect(0, self.titleHeight - self.radius, self.radius, self.radius)
-        titlePath.addRect(self.width - self.radius, self.titleHeight - self.radius, self.radius, self.radius)
+        titlePath.addRoundedRect(0, 0, self.width, self.unitSize, self.radius, self.radius)
+        titlePath.addRect(0, self.unitSize - self.radius, self.radius, self.radius)
+        titlePath.addRect(self.width - self.radius, self.unitSize - self.radius, self.radius, self.radius)
         painter.setPen(Qt.NoPen)
         painter.setBrush(self.titleBrush)
         painter.drawPath(titlePath.simplified())
 
-        # node background
-        contentPath = QPainterPath()
-        contentPath.setFillRule(Qt.WindingFill)
-        contentPath.addRoundedRect(0, self.titleHeight, self.width, self.height - self.titleHeight, self.radius, self.radius)
-        contentPath.addRect(0, self.titleHeight, self.radius, self.radius)
-        contentPath.addRect(self.width - self.radius, self.titleHeight, self.radius, self.radius)
+    def paintBottom(self, painter):
+        bottomPath = QPainterPath()
+        bottomPath.setFillRule(Qt.WindingFill)
+        bottomPath.addRoundedRect(0, 0, self.width, self.height, self.radius, self.radius)
+        # bottomPath.addRect(0, unitStackOffset, self.radius, self.radius)
+        # bottomPath.addRect(self.width - self.radius, unitStackOffset, self.radius, self.radius)
         painter.setPen(Qt.NoPen)
         painter.setBrush(self.backgroundBrush)
-        painter.drawPath(contentPath.simplified())
+        painter.drawPath(bottomPath.simplified())
 
-        # outline
+    def paintOutline(self, painter):
         outlinePath = QPainterPath()
         outlinePath.addRoundedRect(0, 0, self.width, self.height, self.radius, self.radius)
         painter.setPen(self.outlinePenUnselected if not self.isSelected() else self.outlinePenSelected)
         painter.setBrush(Qt.NoBrush)
         painter.drawPath(outlinePath.simplified())
-        
+
+    def paintUnit(self, painter):
+        unitPath = QPainterPath()
+        unitPath.setFillRule(Qt.WindingFill)
+        unitPath.addRect(0, self.unitSize, self.width, self.unitSize)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self.unitBrush)
+        painter.drawPath(unitPath.simplified())
+
+    def paint(self, painter, QStyleOptionGraphicsItem, widget=None):
+        self.paintBottom(painter)
+        self.paintTitle(painter)
+        self.paintUnit(painter)
+        self.paintOutline(painter)
+
     def init(self):
         self.titleElement.setPlainText(self.title)
         self.titleElement.setDefaultTextColor(self.titleColor)
